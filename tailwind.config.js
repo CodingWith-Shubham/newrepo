07d352eb8a75e5fc5921165e800ddd56c7596560
CodingwithShubham <mamgainshubham18@gmail.com>
2024-09-20 21:55:22 +0530

    this file is commited

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        //'custompink': '#1E3A8A',
+      },
+    },
+  },
+  plugins: [],
+}
+
